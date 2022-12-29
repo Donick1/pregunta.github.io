d5d39dbac96ecf209f677299c5de96de28fb6ceb
Donick1 <121593166+Donick1@users.noreply.github.com>
2022-12-29 01:46:54 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const documentReady = () => {
+  const heroTitle = document.querySelector('.hero__title');
+  const heroButtonSi = document.getElementById('heroButtonSi');
+  const heroButtonNo = document.querySelector('#heroButtonNo');
+
+  const nowZomosNobios = () => {
+    alert('Bueno ahora ya somos novios❤️');
+    alert('Hoy, mañana, pasado y siempre si Dios quiere🙏');
+    alert('¿Cuándo la boda bb💍❤️?');
+    location.href = 'https://www.youtube.com/watch?v=sxZuh17L690'
+  };
+
+  const evitarKCRompanMyBobo = () => {
+    heroButtonNo.style.position = 'absolute';
+    heroButtonNo.style.top = (Math.random() * window.innerHeight) + 'px';
+    heroButtonNo.style.left = (Math.random() * window.innerWidth) + 'px';
+  };
+
+  heroButtonSi.addEventListener('click', nowZomosNobios);
+  heroButtonNo.addEventListener('mouseover', evitarKCRompanMyBobo);
+};
+
+document.addEventListener('DOMContentLoaded', documentReady);
